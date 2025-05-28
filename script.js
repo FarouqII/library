4046a81e18dbf455726aeb90080fcaabcec984b8
FarouqII <farouq006@gmail.com>
2025-05-28 03:38:04 +0300

    Set up basic layout
    Set up the basic layout and how the structure of the site will end up like

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const logo = document.getElementById('logo');
+const frame = document.getElementById('frame');
+const mainContent = document.getElementById('main-content');
+
+// Loading Screen
+window.onload = () => {
+    setTimeout(() => {
+      logo.style.transform = "translate(50vw, 50vh) translate(-50%, -375%) scale(0.6)";
+      if (window.innerWidth > 1000) frame.style.opacity = '1';
+      setTimeout(() => mainContent.style.opacity = '1', 50);
+    }, 2000);
+  };
+
+// Owl Animation
+frame.src = "./assets/owl-animation/pixil-frame-0.png";
+
+function blink() {
+  frame.src = "./assets/owl-animation/pixil-frame-1.png";
+
+  setTimeout(() => {
+    frame.src = "./assets/owl-animation/pixil-frame-0.png"
+
+    const nextBlink = Math.floor(Math.random() * 4000) + 2000;
+    setTimeout(blink, nextBlink);
+  }, 150)
+}
+setTimeout(blink, 2000);
+
+// -----------------------------------------------------------
+
 let myLibrary = [];
 
 function Book(id, name, author) {
@@ -15,52 +45,3 @@ function addBookToLibrary(name, author) {
 
 addBookToLibrary("LoTR: Return of The King", "Tolkien");
 addBookToLibrary("The Secret History", "Donna Tartt");
-
-// --------------------------------------------
-
-const logo = document.getElementById('logo');
-const frame = document.getElementById('frame');
-const formContainer = document.getElementById('form-container');
-const submitBtn = document.querySelector('button');
-const password = document.getElementById('password');
-const confirmpw = document.getElementById('confirmPassword');
-const form = document.querySelector('form');
-
-// Loading Screen
-window.onload = () => {
-    setTimeout(() => {
-      if (window.innerWidth >= 768) {
-        logo.style.transform = "translate(50vw, 50vh) translate(-50%, -300%) scale(0.8)";
-      } else if (window.innerWidth < 768) {
-        if (window.innerWidth < 380) {
-        logo.style.transform = "translate(50vw, 50vh) translate(-50%, -305%) scale(0.5)";
-      } else
-        logo.style.transform = "translate(50vw, 50vh) translate(-50%, -200%) scale(0.5)";
-      }
-      if (window.innerWidth > 1000) frame.style.opacity = '1';
-      setTimeout(() => formContainer.style.opacity = '1', 750);
-    }, 2000);
-  };
-
-// Owl Animation
-frame.src = "./assets/owl-animation/pixil-frame-0.png";
-
-function blink() {
-  frame.src = "./assets/owl-animation/pixil-frame-1.png"
-
-  setTimeout(() => {
-    frame.src = "./assets/owl-animation/pixil-frame-0.png"
-
-    const nextBlink = Math.floor(Math.random() * 4000) + 2000;
-    setTimeout(blink, nextBlink);
-  }, 150)
-}
-setTimeout(blink, 2000);
-
-// Form
-form.addEventListener('submit', event => {
-  if (password !== confirmpw) {
-    event.preventDefault();
-    alert("Passwords don't match!");
-  }
-})

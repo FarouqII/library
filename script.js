1b975afb6a2a1f0747081d74c3b6fe9d1b81c6b7
FarouqII <farouq006@gmail.com>
2025-06-08 16:30:45 +0300

    fix readStatus button

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const mainContent = document.getElementById('main-content');
 // Loading Screen
 window.onload = () => {
     setTimeout(() => {
-      logo.style.transform = "translate(50vw, 50vh) translate(-50%, -375%) scale(0.6)";
+      logo.style.transform = "translate(50vw, 50vh) translate(-50%, -315%) scale(0.6)";
       if (window.innerWidth > 1000) frame.style.opacity = '1';
       setTimeout(() => mainContent.style.opacity = '1', 50);
     }, 2000);
@@ -29,7 +29,8 @@ setTimeout(blink, 2000);
 // -----------------------------------------------------------
 
 const gridContainer = document.getElementById('grid-container');
-const addBookBtn = document.getElementById('add-book');
+const addBookContainer = document.getElementById('add-book');
+const addBookBtn = document.getElementById('add-book-btn');
 const addBtn = document.getElementById('add-btn');
 
 let myLibrary = [];
@@ -49,34 +50,52 @@ function addBookToLibrary(name, author) {
     const bookDiv = document.createElement('div');
     bookDiv.className = "book";
 
-    console.log(newBook.name);
-
     bookDiv.innerHTML = `
     <div class="title">${newBook.name}</div>
     <div class="author">${newBook.author}</div>
     <p class="id">${newBook.id}</p>
-    <button class="readStatus unread"></button>`;
+    <button class="readStatus unread" id="status-btn"></button>`;
 
-  gridContainer.appendChild(bookDiv);
+    gridContainer.appendChild(bookDiv);
+
+    const statusBtn = document.getElementById('status-btn');
+    statusBtn.addEventListener("click", () => {
+      if (statusBtn.className.includes("unread")) statusBtn.className = "readStatus read";
+      else statusBtn.className = "readStatus unread";
+    })
 }
 
 addBookBtn.addEventListener('click', () => {
-  addBookBtn.innerHTML = `
-    <form action="#" method="POST">
+  addBookContainer.className = "book add form";
+  addBookContainer.innerHTML = `
+    <form id="book-form">
         <label for="add-title">Title:</label>
-        <input type="text" id="add-title" for="add-title">
+        <input type="text" id="add-title" autocomplete="off">
 
         <label for="add-author">Author:</label>
-        <input type="text" id="add-author" for="add-author">
+        <input type="text" id="add-author" autocomplete="off">
 
         <button type="submit" id="add-btn">Add +</button>
-        <button id="cancel-btn">Cancel</button>
+        <button type="button" id="cancel-btn">Cancel</button>
     </form>`;
-    addBookBtn.className = "book add form";
+
+    const form = document.getElementById('book-form');
+    const cancelBtn = document.getElementById('cancel-btn');
+
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const title = document.getElementById('add-title').value.trim();
+      const author  = document.getElementById('add-author').value.trim();
+
+      if (!title || !author) return;
+
+      addBookToLibrary(title, author);
+      resetAddContainer();
+    })
+    cancelBtn.addEventListener('click', resetAddContainer);
 })
 
-addBtn.addEventListener("click", () => {
-  let newTitle = document.getElementById("add-title").value;
-  let newAuthor = document.getElementById("add-author").value;
-  addBookToLibrary(newTitle, newAuthor);
-})
+function resetAddContainer() {
+  addBookContainer.className = "book add";
+  addBookContainer.innerHTML = "<button id=\"add-book-btn\">Add Book</button>";
+}
